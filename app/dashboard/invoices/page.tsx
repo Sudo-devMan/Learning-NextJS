323c7be8c21307c665a0291d1338f9039016034d
Devman <inevitabledouble.e@gmail.com>
2026-04-05 11:10:55 +0200

    Add dashboard route

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Page() {
+    return (
+        <div>
+            <h1 className="text-cenetr m-20 text-3xl text-yellow-700">
+                Invoices page bruh
+            </h1>
+        </div>
+    )
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 
 export default function Page() {
-    return 
+    return(
         <div>
-            <h1 className="text-green-600 text-3xl">
+            <h1 className="text-center m-20 text-green-600 text-3xl">
                 Broski in da Dashboard
             </h1>
         </div>
+    ) 
 }
